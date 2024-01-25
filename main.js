324642ec83a8e2e15a158503492c0014bbc8f57a
Ben <bengeraghty.bg@gmail.com>
2024-01-25 12:06:16 +0700

    add $ to ouput value

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const slider = document.getElementById("myRange");
 const output = document.getElementById("output");
-output.textContent = slider.value; // Display the default slider value
+
+
+output.textContent = '$' + slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  output.textContent = this.value;
+  output.textContent = '$' + this.value;
 }
+
