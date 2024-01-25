bc5940bfc259b291e55e4d1b756b15a815592a66
Ben <bengeraghty.bg@gmail.com>
2024-01-25 12:29:21 +0700

    change UI layout order

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const slider = document.getElementById("myRange");
 const output = document.getElementById("output");
 const initialNum = document.querySelector('.initial-number__value');
+const messageBox = document.querySelector('.message');
 
 
 output.textContent = '$' + slider.value; // Display the default slider value
@@ -15,12 +16,12 @@ let account = 1000;
 
 function init() {
   console.log('game started');
+  messageBox.textContent = 'Higher or Lower?'
   let randomNumber = (Math.floor(Math.random() * 100) + 1);
-  console.log(randomNumber);
   initialNum.textContent = randomNumber;
 
 }
-// generate a random number in initial number display box
+
 
 
 // on press get bet amount from output value
