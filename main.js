3ca36955d3d6d3113d18c622d64bdf0d9954b4c5
Ben <bengeraghty.bg@gmail.com>
2024-01-25 12:16:14 +0700

    add random initial number

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const slider = document.getElementById("myRange");
 const output = document.getElementById("output");
+const initialNum = document.querySelector('.initial-number__value');
 
 
 output.textContent = '$' + slider.value; // Display the default slider value
@@ -9,3 +10,21 @@ slider.oninput = function() {
   output.textContent = '$' + this.value;
 }
 
+let account = 1000;
+
+
+function init() {
+  console.log('game started');
+  let randomNumber = (Math.floor(Math.random() * 100) + 1);
+  console.log(randomNumber);
+  initialNum.textContent = randomNumber;
+
+}
+// generate a random number in initial number display box
+
+
+// on press get bet amount from output value
+
+// store bet amount in variable and generate a random number
+
+init();
