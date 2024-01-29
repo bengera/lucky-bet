94636ec4d8b3f33dee2e5415a164124c826657ab
Ben <bengeraghty.bg@gmail.com>
2024-01-29 11:45:22 +0700

    update slider range based on account amount

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
+let account = 2000;
+
 const slider = document.getElementById("myRange");
 const output = document.getElementById("output");
 const initialNum = document.querySelector('.initial-number__value');
 const messageBox = document.querySelector('.message');
 
+//Update the slider value based on account number
+const accountValueEl = document.querySelector('.account__value');
+accountValueEl.textContent = '$ ' + account;
+const accountValue = accountValueEl.textContent;
+const accountValueAmount = Number(accountValue.replace('$',''));
+slider.max = accountValueAmount;
 
 output.textContent = '$' + slider.value; // Display the default slider value
 
@@ -10,8 +18,6 @@ output.textContent = '$' + slider.value; // Display the default slider value
 slider.oninput = function() {
   output.textContent = '$' + this.value;
 }
-
-let account = 1000;
 
 
 function init() {
@@ -22,10 +28,5 @@ function init() {
 
 }
 
-
-
-// on press get bet amount from output value
-
-// store bet amount in variable and generate a random number
 
 init();
